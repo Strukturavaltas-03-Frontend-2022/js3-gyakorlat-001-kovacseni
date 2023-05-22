5495541878c42d9a355877c9cd903a8d56bbaa78
kovacseni <kovacseni@gmail.com>
2023-05-22 21:11:10 +0200

    3 feladat kész

--- 08-other/exercises/02/solution/js/HU.js
+++ 08-other/exercises/02/solution/js/HU.js
@@ -1,1 +1,15 @@
+
+
+const HU = {
+    date(dateParam) {
+        return new Intl.DateTimeFormat('hu-HU').format(dateParam);
+    },
+    currency(currencyParam) {
+        return new Intl.NumberFormat('hu-HU', {style: 'currency', currency: 'HUF'}).format(currencyParam);
+    },
+    list(stringArray) {
+        return `${stringArray[0]}, ${stringArray[1]} és ${stringArray[2]}`;
+    }
+}
+
 export default HU;
--- 08-other/exercises/03/solution/js/objectConverter.js
+++ 08-other/exercises/03/solution/js/objectConverter.js
@@ -1,1 +1,47 @@
+
+const objectConverter = {
+    arrayToMap(array) {
+        let result = new Map();
+        for (let i = 0; i < array.length; i++) {
+            result.set(i, array[i]);
+        }
+        return result;
+    },
+    arrayToSet(array) {
+        return new Set(array);
+    },
+    setToMap(set) {
+        let result = new Map();
+        let values = set.values();
+        for (let i = 0; i < set.size; i++) {
+            result.set(i, values.next().value);
+        }
+        return result;
+    },
+    setToArray(set) {
+        let result = [];
+        let values = set.values();
+        for (let i = 0; i < set.size; i++) {
+            result.push(values.next().value);
+        }
+        return result;
+    },
+    mapToArray(map) {
+        let result = [];
+        let keys = map.keys();
+        for (let i = 0; i < map.size; i++) {
+            result.push(map.get(keys.next().value));
+        }
+        return result;
+    },
+    mapToSet(map) {
+        let result = new Set;
+        let keys = map.keys();
+        for (let i = 0; i < map.size; i++) {
+            result.add(map.get(keys.next().value));
+        }
+        return result;
+    }
+};
+
 export default objectConverter;

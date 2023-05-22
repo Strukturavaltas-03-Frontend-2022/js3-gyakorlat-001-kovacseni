86070a16d0e5569ebda60976525b62c44f2c2be8
kovacseni <kovacseni@gmail.com>
2023-05-22 21:24:08 +0200

    kész

--- 08-other/exercises/04/solution/js/customDateFormats.js
+++ 08-other/exercises/04/solution/js/customDateFormats.js
@@ -1,1 +1,17 @@
+
+function getShortDate(date) {
+    return new Intl.DateTimeFormat('hu-HU', {dateStyle: 'medium', timeStyle: 'short'}).format(date);
+}
+
+function getLongDate(date) {
+    return new Intl.DateTimeFormat('hu-HU', {dateStyle: 'long', timeStyle: 'medium'}).format(date);
+}
+
+function customDateFormats(date) {
+    return {
+        short: getShortDate(date),
+        long: getLongDate(date)
+    };
+}
+
 export default customDateFormats;
